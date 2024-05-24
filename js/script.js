1ca22bdf6bd03e2991bd02735493b2574ce1a8b8
sanmacorz <sanmacorz@protonmail.com>
2024-05-23 21:22:00 -0600

    Agregando los archivos multimedia y terminando segmentos de archivos principales

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Sticky navbar
+// Navbar sticky
 window.onscroll = function () {
     stickyNavbar()
 };
@@ -13,7 +13,7 @@ function stickyNavbar() {
     }
 }
 
-// Image carousel
+// Carousel de imagenes
 $(document).ready(function () {
     $('.carousel').slick({
         infinite: true,
@@ -25,3 +25,44 @@ $(document).ready(function () {
         dots: true
     });
 });
+
+// Mensaje formulario
+function mensaje_formulario(form) {
+    window.alert("¡Su sesión ha sido agendada correctamente!\nPor favor cumpla con la hora y fecha seleccionada.");
+}
+
+// buscarTabla
+function buscarTabla() {
+    let input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+// filtrarLista
+function filtrarLista() {
+    const dropdown = document.getElementById('nameDropdown');
+    const selectedName = dropdown.value;
+    const listItems = document.querySelectorAll('#nameList li');
+
+    listItems.forEach(item => {
+        if (selectedName === 'Todos' || item.getAttribute('data-name') === selectedName) {
+            item.style.display = 'list-item';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
